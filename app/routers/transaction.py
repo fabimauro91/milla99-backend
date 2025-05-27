22b37f4e35df8f98b76fa1b2b27023fab0d24bf7
Daniel Vargas <dafvargasro.tech@gmail.com>
2025-05-26 19:09:54 -0500

    Add authentication to transaction endpoints
    - Implement HTTPBearer security for the create_transaction, get_my_balance, and list_my_transactions endpoints to ensure user authentication.
    - Update function signatures to include credentials parameter for enhanced security and access control.

--- app/routers/transaction.py
+++ app/routers/transaction.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter, Depends, Request
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi import Security
 from app.core.db import SessionDep
 from app.services.transaction_service import TransactionService
 from app.models.transaction import TransactionType, TransactionCreate
 
 router = APIRouter(prefix="/transactions", tags=["transactions"])
+
+bearer_scheme = HTTPBearer()
 
 
 @router.post("/", description="Crea una transacción para el usuario autenticado.")
@@ -23,14 +27,14 @@ def create_transaction(
 
 
 @router.get("/balance/me", description="Consulta el saldo (available, withdrawable y mount) del usuario autenticado.")
-def get_my_balance(request: Request, session: SessionDep):
+def get_my_balance(request: Request, session: SessionDep, credentials: HTTPAuthorizationCredentials = Security(bearer_scheme)):
     user_id = request.state.user_id  # <-- lo tomas del token
     service = TransactionService(session)
     return service.get_user_balance(user_id)
 
 
 @router.get("/list/me", description="Lista todas las transacciones del usuario autenticado.")
-def list_my_transactions(request: Request, session: SessionDep):
+def list_my_transactions(request: Request, session: SessionDep, credentials: HTTPAuthorizationCredentials = Security(bearer_scheme)):
     user_id = request.state.user_id  # <-- lo tomas del token
     service = TransactionService(session)
     return service.list_transactions(user_id)
